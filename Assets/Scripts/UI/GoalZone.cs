dfefa57ca6cdc8f7c792fae7bf452d270a6e8770
Abolfazl-Dashti <155985742+Abolfazl-Dashti@users.noreply.github.com>
2026-08-04 10:47:26 +0330

    implement bot nav agents

--- Assets/Scripts/UI/GoalZone.cs
+++ Assets/Scripts/UI/GoalZone.cs
@@ -4,28 +4,33 @@ using TMPro;
 
 public class GoalZone : MonoBehaviour
 {
-    // پارتیکل سیستمی که باید پلی بشه
+    [Header("VFX & UI")]
     public ParticleSystem goalParticle;
-    // پارتیکل سیستمی که باید پلی بشه
     public ParticleSystem goalParticle1;
-    // تکست UI که شمارنده رو نشون میده
-    public TMP_Text scoreText;
+    public TMP_Text scoreText;  // تکست UI که شمارنده رو نشون میده
 
-    // لایه‌ای که فقط دیسک داره - حساسه، اشتباه بدی کار نمیکنه
-    public LayerMask Disc;
-
-    private int score = 0;
-
-    private void OnTriggerEnter(Collider other)
+    public void PlayGoalEffect()
     {
-        // چک میکنیم ابجکت ورودی لایه disc داره یا نه
-        if ((Disc.value & (1 << other.gameObject.layer)) == 0) return;
-
-        goalParticle.Play();
-        goalParticle1.Play();
-
-        score++;
-        scoreText.text = score.ToString();
+        if (goalParticle) goalParticle.Play();
+        if (goalParticle1) goalParticle1.Play();
     }
+    
+    
+    // لایه‌ای که فقط دیسک داره - حساسه، اشتباه بدی کار نمیکنه
+    // public LayerMask Disc;
+
+    // private int score = 0;
+
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     // چک میکنیم ابجکت ورودی لایه disc داره یا نه
+    //     if ((Disc.value & (1 << other.gameObject.layer)) == 0) return;
+    //
+    //     goalParticle.Play();
+    //     goalParticle1.Play();
+    //
+    //     score++;
+    //     scoreText.text = score.ToString();
+    // }
 }
 
