791e8b24af336165294c823a5b896592503c645a
aliataei2 <aliataei4000@gmail.com>
2026-08-25 22:56:53 -0800

    menu

---
+++ Assets/Scripts/menu/menu.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    void Update()
+    {
+        if (Keyboard.current != null &&
+            Keyboard.current.mKey.wasPressedThisFrame)
+        {
+            home();
+        }
+    }
+
+    public void play()//تابع دکمه شروع بازی
+    {
+        print("playyyy");
+        SceneManager.LoadScene("Prototype-bakup 2");
+        //کد لود کردن یک سین . باید اسم سین یا همون لول رو بنویسیم
+    }
+    public void exit()//تابع خروج از بازی
+    {
+        print("exittttttttt");
+        Application.Quit();
+        //دستور خارج شدن از برنامه و بسته شدن برنامه
+    }
+    public void home()//تابع دکمه هوم
+    {
+        SceneManager.LoadScene("menu");
+        //کد لود کردن یک سین . باید اسم سین یا همون لول رو بنویسیم
+    }
+}
